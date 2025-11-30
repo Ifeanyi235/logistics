045cfe82d54f2e4b67e03f8359c04b81495e58d0
ifeanyi235 <ichuku2352gmail.com>
2025-11-30 12:33:44 +0100

    adjusted image size

--- src/components/bphil.jsx
+++ src/components/bphil.jsx
@@ -19,7 +19,7 @@ function Bphil (props) {
                 opacity : 1,
                 x : 0
             }} viewport={{
-                once : false
+                once : true
             }} transition={{
                 duration : 0.8,
                 delay : 0.2
